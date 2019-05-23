e8c10c59a6690d87a71f65a40f87143d013695db
Berberer <lukas.brandt@veds.de>
2019-05-23 18:53:25 +0200

    probabilities are now allowed, which do not add up to 1

--- src/Schedule.py
+++ src/Schedule.py
@@ -6,11 +6,14 @@ class Schedule(object):
         super(Schedule, self).__init__()
         self._permutation_matrices = permutation_matrices
         self._probabilities = probabilities
+        self._max_probability = 0
+        for p in self._probabilities:
+            self._max_probability = self._max_probability + p
 
     def get_random_permutation_matrix(self):
         index = 0
         sum = 0
-        chance = random.uniform(0, 1)
+        chance = random.uniform(0, self._max_probability)
         for index, probability in enumerate(self._probabilities):
             sum = sum + probability
             if chance <= sum:
