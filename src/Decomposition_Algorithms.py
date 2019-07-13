829e5d3be1abfd38b47a62714e049b6a2ac6f520
Mirko Juergens <mirkoj@mail.upb.de>
2019-07-14 00:10:12 +0200

    implements DOUBLE

--- src/Decomposition_Algorithms.py
+++ src/Decomposition_Algorithms.py
@@ -95,3 +95,140 @@ def qbvn(traffic_matrix):
     unique, counts = np.unique(permutation_matrices,axis=0, return_counts=True)
     probabilities = counts / np.sum(counts)
     return unique, probabilities
+
+# Algorithm 3
+def double(traffic_matrix):
+    # In this paper C is a cumulative request matrix measured over a period of time
+    C = traffic_matrix
+    # 1. Define A s.t. a_ij = floor(c_ij / T/N)
+    N = len(C)
+    # T = #of batches -> we only send 1 packet
+    T = 1
+    A = C * (N/T)
+    A = np.floor(A)
+ #   print("Blown up T")
+ #   print (str(A))
+    # We now color the graph iteratively, since mirko is too lazy to look for an appropirate algorithm, we use the matching one:
+    # Each matching represents one color, after such a matching has been found, we remove the matching from the (multi-) graph and
+    # look for a new matching
+
+
+    # Create biparite multi graph from A
+    bpGraph = {}
+    for lineNum, line in enumerate(A):
+        currDestinations = list()
+        for elementNum, element in enumerate(line):
+            for i in range(0, int(element)):
+                currDestinations.append(elementNum)
+        bpGraph[lineNum] = currDestinations
+#   print("Biparite Graph:")
+#    print(bpGraph)
+    
+    permutation_matrices = []
+    # As long as we did not split up A entirely
+    while any (len(x) > 0 for x in bpGraph.values()):
+ #       print("Biparite graph:")
+ #       print(bpGraph)
+        matching = {}
+        (matching, a, u) = bipartiteMatch(bpGraph)
+#        print("Matching:")
+#        print(matching)
+        # Each matching/color corresponds to a permutation matrix
+        # Create permutation matrix and remove edge from biparite graph
+        permutation_matrix = np.zeros(traffic_matrix.shape, dtype="int")
+        for output_port, input_port in matching.items():
+            permutation_matrix[input_port][output_port] = 1
+            bpGraph[input_port].remove(output_port)
+        permutation_matrices.append(permutation_matrix)
+    
+    # Now the final step will produce N arbitrary permutations:
+    for counter in range(0, N):
+        permutation_matrix = np.zeros(traffic_matrix.shape, dtype="int")
+        free_destinations = list(range(0, N))
+        for input_port in range(0 , N):
+            output_port = np.random.choice(free_destinations)
+            free_destinations.remove(output_port)
+            permutation_matrix[input_port][output_port] = 1
+        permutation_matrices.append(permutation_matrix)
+
+    probabilities = [1/len(permutation_matrices) for i in range(0, len(permutation_matrices))]
+    return permutation_matrices, probabilities
+    
+
+# Copyright: http://code.activestate.com/recipes/123641-hopcroft-karp-bipartite-matching/
+# Hopcroft-Karp bipartite max-cardinality matching and max independent set
+# David Eppstein, UC Irvine, 27 Apr 2002
+def bipartiteMatch(graph):
+	'''Find maximum cardinality matching of a bipartite graph (U,V,E).
+	The input format is a dictionary mapping members of U to a list
+	of their neighbors in V.  The output is a triple (M,A,B) where M is a
+	dictionary mapping members of V to their matches in U, A is the part
+	of the maximum independent set in U, and B is the part of the MIS in V.
+	The same object may occur in both U and V, and is treated as two
+	distinct vertices if this happens.'''
+	
+	# initialize greedy matching (redundant, but faster than full search)
+	matching = {}
+	for u in graph:
+		for v in graph[u]:
+			if v not in matching:
+				matching[v] = u
+				break
+	
+	while 1:
+		# structure residual graph into layers
+		# pred[u] gives the neighbor in the previous layer for u in U
+		# preds[v] gives a list of neighbors in the previous layer for v in V
+		# unmatched gives a list of unmatched vertices in final layer of V,
+		# and is also used as a flag value for pred[u] when u is in the first layer
+		preds = {}
+		unmatched = []
+		pred = dict([(u,unmatched) for u in graph])
+		for v in matching:
+			del pred[matching[v]]
+		layer = list(pred)
+		
+		# repeatedly extend layering structure by another pair of layers
+		while layer and not unmatched:
+			newLayer = {}
+			for u in layer:
+				for v in graph[u]:
+					if v not in preds:
+						newLayer.setdefault(v,[]).append(u)
+			layer = []
+			for v in newLayer:
+				preds[v] = newLayer[v]
+				if v in matching:
+					layer.append(matching[v])
+					pred[matching[v]] = v
+				else:
+					unmatched.append(v)
+		
+		# did we finish layering without finding any alternating paths?
+		if not unmatched:
+			unlayered = {}
+			for u in graph:
+				for v in graph[u]:
+					if v not in preds:
+						unlayered[v] = None
+			return (matching,list(pred),list(unlayered))
+
+		# recursively search backward through layers to find alternating paths
+		# recursion returns true if found path, false otherwise
+		def recurse(v):
+			if v in preds:
+				L = preds[v]
+				del preds[v]
+				for u in L:
+					if u in pred:
+						pu = pred[u]
+						del pred[u]
+						if pu is unmatched or recurse(pu):
+							matching[v] = u
+							return 1
+			return 0
+
+		for v in unmatched: recurse(v)
+    
+    
+    
--- src/Simulator.py
+++ src/Simulator.py
@@ -1,6 +1,6 @@
 import json
 import click
-from Decomposition_Algorithms import gljd,exact,qbvn
+from Decomposition_Algorithms import gljd,exact,qbvn,double
 from Simulation_Run import Run
 from Evaluation import Evaluation
 
@@ -8,7 +8,8 @@ from Evaluation import Evaluation
 heuristics = {
     "GLJD": gljd,
     "EXACT": exact,
-    "QBVN": qbvn
+    "QBVN": qbvn,
+    "DOUBLE":double
 }
 
 @click.command()
