1b2942a501d02bc245e1e002fceb778ec96041b6
Berberer <lukas.brandt@veds.de>
2019-07-17 23:29:51 +0200

    added plot for throughput with different max queue lengths

--- vis/Plot.py
+++ vis/Plot.py
@@ -16,28 +16,52 @@ colors = {
     "DOUBLE": (1, 0, 1, 0.5)
 }
 
-heuristic_data = {}
-for h in heuristics:
-    heuristic_data[h] = simulation_results[simulation_results["heuristic"] == h]
-
 def query_data(columns_clause, where_clause):
     query = "SELECT {} FROM results WHERE {}".format(columns_clause, where_clause)
     return zip(*engine.execute(query).fetchall())
 
-def plot_line_with_std_deviation(simulation_length, x, x_label, y, y_label, file_name):
+def plot_line_with_std_deviation(simulation_length, max_queue_length, x, x_label, y, y_label, file_name):
     pyplot.figure()
     pyplot.xlabel(x_label)
     pyplot.ylabel(y_label)
-    for heuristic, data in heuristic_data.items():
-        columns_clause = "{}, {}_average, {}_variance".format(x, y, y)
-        where_clause =  "heuristic='{}' AND simulation_length={}".format(heuristic, simulation_length)
+    columns_clause = "{}, {}_average, {}_variance".format(x, y, y)
+    for heuristic in heuristics:
+        where_clause =  "heuristic='{}' AND simulation_length={} AND max_queue_length={}".format(heuristic, simulation_length, max_queue_length)
         sizes, averages, variances = query_data(columns_clause, where_clause)
         pyplot.errorbar(sizes, averages, yerr=numpy.sqrt(variances), color=colors[heuristic], label=heuristic)
     pyplot.legend()
     pyplot.tight_layout()
     pyplot.savefig("vis/{}".format(file_name))
 
-plot_line_with_std_deviation(100, "switch_size", "Switch Size", "throughput", "Throughput", "SwitchSizeToThroughput.png")
-plot_line_with_std_deviation(100, "switch_size", "Switch Size", "permutation_matrix_amount", "Number of Permutation Matrices", "SwitchSizeToMatrixNumber.png")
-plot_line_with_std_deviation(100, "switch_size", "Switch Size", "queue_length", "Average Queue Length", "SwitchSizeToQueueLength.png")
-plot_line_with_std_deviation(100, "switch_size", "Switch Size", "packet_delay", "Average Packet Delay", "SwitchSizeToPacketDelay.png")
+def plot_heuristic_throughput_for_different_queue_lengths(simulation_length, heuristic, max_queue_lengths, file_name):
+    max_queue_lengths.sort(reverse=True)
+    pyplot.figure()
+    pyplot.xlabel("Switch Size")
+    pyplot.ylabel("Throughput")
+    columns_clause = "switch_size, throughput_average, throughput_variance"
+    for i, max_queue_length in enumerate(max_queue_lengths):
+        where_clause =  "heuristic='{}' AND simulation_length={} AND max_queue_length={}".format(heuristic, simulation_length, max_queue_length)
+        sizes, averages, variances = query_data(columns_clause, where_clause)
+        c = colors[heuristic]
+        r = float(i) / float(len(max_queue_lengths))
+        if c[0]==0:
+            c = (r, c[1], c[2], c[3])
+        if c[1]==0:
+            c = (c[0], r, c[2], c[3])
+        if c[2]==0:
+            c = (c[0], c[1], r, c[3])
+        pyplot.errorbar(sizes, averages, yerr=numpy.sqrt(variances), color=c, label="<= {}".format(str(max_queue_length)))
+    pyplot.legend()
+    pyplot.tight_layout()
+    pyplot.savefig("vis/{}".format(file_name))
+
+plot_line_with_std_deviation(100, 50, "switch_size", "Switch Size", "throughput", "Throughput", "SwitchSizeToThroughput.png")
+plot_line_with_std_deviation(100, 50, "switch_size", "Switch Size", "permutation_matrix_amount", "Number of Permutation Matrices", "SwitchSizeToMatrixNumber.png")
+plot_line_with_std_deviation(100, 50, "switch_size", "Switch Size", "queue_length", "Average Queue Length", "SwitchSizeToQueueLength.png")
+plot_line_with_std_deviation(100, 50, "switch_size", "Switch Size", "packet_delay", "Average Packet Delay", "SwitchSizeToPacketDelay.png")
+
+plot_heuristic_throughput_for_different_queue_lengths(100, "GLJD", [10, 25, 50], "MaxQueueLengthsGLJD.png")
+plot_heuristic_throughput_for_different_queue_lengths(100, "QBVN", [10, 25, 50], "MaxQueueLengthsQBVN.png")
+plot_heuristic_throughput_for_different_queue_lengths(100, "QBVN_Cover", [10, 25, 50], "MaxQueueLengthsQBVN_Cover.png")
+plot_heuristic_throughput_for_different_queue_lengths(100, "EXACT", [10, 25, 50], "MaxQueueLengthsEXACT.png")
+plot_heuristic_throughput_for_different_queue_lengths(100, "DOUBLE", [10, 25, 50], "MaxQueueLengthsDOUBLE.png")
