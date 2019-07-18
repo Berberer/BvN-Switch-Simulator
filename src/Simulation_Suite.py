ee9cff6398dac0259d83141c46815a95c9c6c6f7
Mirko Juergens <mirkoj@mail.upb.de>
2019-07-18 09:30:56 +0200

    Fixes missing load in csv header

--- src/Simulation_Suite.py
+++ src/Simulation_Suite.py
@@ -15,7 +15,7 @@ switch_sizes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25]
 max_queue_lengths = [10,25,50]
 simulation_lengths = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 
-header = "heuristic,switch_size,max_queue_length,simulation_length,packet_delay_average,packet_delay_variance,packet_delay_max,packet_delay_min,queue_length_average,queue_length_variance,queue_length_max,queue_length_min,permutation_matrix_amount_average,permutation_matrix_amount_variance,permutation_matrix_amount_max,permutation_matrix_amount_min,throughput_average,throughput_variance,throughput_max,throughput_min"
+header = "heuristic,switch_size,max_queue_length,simulation_length,load,packet_delay_average,packet_delay_variance,packet_delay_max,packet_delay_min,queue_length_average,queue_length_variance,queue_length_max,queue_length_min,permutation_matrix_amount_average,permutation_matrix_amount_variance,permutation_matrix_amount_max,permutation_matrix_amount_min,throughput_average,throughput_variance,throughput_max,throughput_min"
 
 with open("data/results.csv", "w+") as file:
     file.write(header + "\n")
